400ca26c485b7ff9b8b4cc4d0ba52a2ea489b781
Harshit <av.harshith15@gmail.com>
2021-08-28 18:53:52 +0530

    basic-template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TezosToolkit } from "@taquito/taquito";
+import "./App.css";
+import ConnectButton from "./components/ConnectWallet";
+import DisconnectButton from "./components/DisconnectWallet";
+import UpdateContract from "./components/UpdateContract";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum BeaconConnection {
+  NONE = "",
+  LISTENING = "Listening to P2P channel",
+  CONNECTED = "Channel connected",
+  PERMISSION_REQUEST_SENT = "Permission request sent, waiting for response",
+  PERMISSION_REQUEST_SUCCESS = "Wallet is connected"
 }
+
+const App = () => {
+  const [Tezos, setTezos] = useState<TezosToolkit>(
+    new TezosToolkit("https://api.tez.ie/rpc/granadanet")
+  );
+  const [contract, setContract] = useState<any>(undefined);
+  const [publicToken, setPublicToken] = useState<string | null>("");
+  const [wallet, setWallet] = useState<any>(null);
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [userBalance, setUserBalance] = useState<number>(0);
+  const [storage, setStorage] = useState<number>(0);
+  const [copiedPublicToken, setCopiedPublicToken] = useState<boolean>(false);
+  const [beaconConnection, setBeaconConnection] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>("transfer");
+
+  const contractAddress: string = "KT1WpkbcHe3UzVpwn95cqWuav8aPMR73bW6p";
+
+  if (publicToken && (!userAddress || isNaN(userBalance))) {
+    return (
+      <div className="main-box">
+        <h1>Taquito Boilerplate</h1>
+        <div id="dialog">
+          <header>Try the Taquito Boilerplate App!</header>
+          <div id="content">
+            <p className="text-align-center">
+              <i className="fas fa-broadcast-tower"></i>&nbsp; Connecting to
+              your wallet
+            </p>
+            <p id="public-token">
+              {copiedPublicToken ? (
+                <span id="public-token-copy__copied">
+                  <i className="far fa-thumbs-up"></i>
+                </span>
+              ) : (
+                <span
+                  id="public-token-copy"
+                  onClick={() => {
+                    if (publicToken) {
+                      navigator.clipboard.writeText(publicToken);
+                      setCopiedPublicToken(true);
+                      setTimeout(() => setCopiedPublicToken(false), 2000);
+                    }
+                  }}
+                >
+                  <i className="far fa-copy"></i>
+                </span>
+              )}
+
+              <span>
+                Public token: <span>{publicToken}</span>
+              </span>
+            </p>
+            <p className="text-align-center">
+              Status: {beaconConnection ? "Connected" : "Disconnected"}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (userAddress && !isNaN(userBalance)) {
+    return (
+      <div className="main-box">
+        <h1>Template</h1>
+        <div id="dialog">
+          <div id="content">
+              <div id="increment-decrement">
+                <h3 className="text-align-center">
+                  Current counter: <span>{storage}</span>
+                </h3>
+                <UpdateContract
+                  contract={contract}
+                  setUserBalance={setUserBalance}
+                  Tezos={Tezos}
+                  userAddress={userAddress}
+                  setStorage={setStorage}
+                />
+              </div>
+            <p>
+              <i className="far fa-file-code"></i>&nbsp;
+              <a
+                href={`https://better-call.dev/granadanet/${contractAddress}/operations`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {contractAddress}
+              </a>
+            </p>
+            <p>
+              <i className="far fa-address-card"></i>&nbsp; {userAddress}
+            </p>
+            <p>
+              <i className="fas fa-piggy-bank"></i>&nbsp;
+              {(userBalance / 1000000).toLocaleString("en-US")} ꜩ
+            </p>
+          </div>
+          <DisconnectButton
+            wallet={wallet}
+            setPublicToken={setPublicToken}
+            setUserAddress={setUserAddress}
+            setUserBalance={setUserBalance}
+            setWallet={setWallet}
+            setTezos={setTezos}
+            setBeaconConnection={setBeaconConnection}
+          />
+        </div>
+      </div>
+    );
+  } else if (!publicToken && !userAddress && !userBalance) {
+    return (
+      <div className="main-box">
+        <div className="title">
+          <h1>Template</h1>
+        </div>
+        <div id="dialog">
+          <header>Basic Template</header>
+          <div id="content">
+            <p>Test</p>
+          </div>
+          <ConnectButton
+            Tezos={Tezos}
+            setContract={setContract}
+            setPublicToken={setPublicToken}
+            setWallet={setWallet}
+            setUserAddress={setUserAddress}
+            setUserBalance={setUserBalance}
+            setStorage={setStorage}
+            contractAddress={contractAddress}
+            setBeaconConnection={setBeaconConnection}
+            wallet={wallet}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return <div>An error has occurred</div>;
+  }
+};
 
 export default App;
