44b18f229b5416530423d93eaeb481d7f17e03f5
srijan-paul <srijannr1@gmail.com>
2021-08-31 21:29:03 +0530

    updated about section UI

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -60,8 +60,8 @@ function Hero() {
 }
 
 type CanvasBounds = {
-  width: 200;
-  height: 200;
+  width: number;
+  height: number;
 };
 
 function HoundPlay() {
@@ -133,21 +133,15 @@ function Home() {
   return (
     <div>
       <Hero />
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 170">
-        <path
-          fill="#edf2f8"
-          fillOpacity="1"
-          d="M0,96L80,101.3C160,107,320,117,480,112C640,107,800,85,960,80C1120,75,1280,85,1360,90.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#1a73e8" fill-opacity="1" d="M0,256L1440,192L1440,320L0,320Z"></path>
+        </svg>
       </svg>
 
       <AboutGame />
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#edf2f8"
-          fillOpacity="1"
-          d="M0,64L120,58.7C240,53,480,43,720,42.7C960,43,1200,53,1320,58.7L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
-        ></path>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
+        <path fill="#1a73e8" fill-opacity="1" d="M0,160L1440,64L1440,0L0,0Z"></path>
       </svg>
     </div>
   );
