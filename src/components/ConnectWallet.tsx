0fd22ba743485cb26787b985df9de0079942ddfc
srijan-paul <srijannr1@gmail.com>
2021-08-31 16:52:22 +0530

    formatted all files and added houndgen

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -6,67 +6,67 @@ import Button from "./Button";
 import { WalletContext } from "./context/WalletContext";
 
 type ButtonProps = {
-	Tezos: TezosToolkit;
-	setPublicToken: Dispatch<SetStateAction<string | null>>;
-	setWalletConnected: Dispatch<SetStateAction<boolean>>;
+  Tezos: TezosToolkit;
+  setPublicToken: Dispatch<SetStateAction<string | null>>;
+  setWalletConnected: Dispatch<SetStateAction<boolean>>;
 };
 
 const ConnectButton = ({ Tezos, setPublicToken, setWalletConnected }: ButtonProps): JSX.Element => {
-	const walletInfo = useContext(WalletContext);
+  const walletInfo = useContext(WalletContext);
 
-	const connectWallet = async () => {
-		const wallet = walletInfo.wallet as BeaconWallet;
-		try {
-			await wallet.requestPermissions({
-				network: {
-					type: NetworkType.GRANADANET,
-					rpcUrl: "https://api.tez.ie/rpc/granadanet",
-				},
-			});
-			const userAddress = await wallet.getPKH();
-			walletInfo.setAddress(userAddress);
-			setWalletConnected(true);
-			console.log(walletInfo.userAddress);
-		} catch (error) {
-			console.error(error);
-			setWalletConnected(false);
-		}
-	};
+  const connectWallet = async () => {
+    const wallet = walletInfo.wallet as BeaconWallet;
+    try {
+      await wallet.requestPermissions({
+        network: {
+          type: NetworkType.GRANADANET,
+          rpcUrl: "https://api.tez.ie/rpc/granadanet",
+        },
+      });
+      const userAddress = await wallet.getPKH();
+      walletInfo.setAddress(userAddress);
+      setWalletConnected(true);
+      console.log(walletInfo.userAddress);
+    } catch (error) {
+      console.error(error);
+      setWalletConnected(false);
+    }
+  };
 
-	useEffect(() => {
-		(async () => {
-			const wallet = new BeaconWallet({
-				name: "Taquito Boilerplate",
-				preferredNetwork: NetworkType.GRANADANET,
-				disableDefaultEvents: true,
-				eventHandlers: {
-					[BeaconEvent.PAIR_INIT]: {
-						handler: defaultEventCallbacks.PAIR_INIT,
-					},
-					[BeaconEvent.PAIR_SUCCESS]: {
-						handler: data => setPublicToken(data.publicKey),
-					},
-				},
-			});
+  useEffect(() => {
+    (async () => {
+      const wallet = new BeaconWallet({
+        name: "Taquito Boilerplate",
+        preferredNetwork: NetworkType.GRANADANET,
+        disableDefaultEvents: true,
+        eventHandlers: {
+          [BeaconEvent.PAIR_INIT]: {
+            handler: defaultEventCallbacks.PAIR_INIT,
+          },
+          [BeaconEvent.PAIR_SUCCESS]: {
+            handler: data => setPublicToken(data.publicKey),
+          },
+        },
+      });
 
-			// TODO (@srijan): why do I have to cast the wallet twice here?
-			Tezos.setWalletProvider(wallet as unknown as WalletProvider);
+      // TODO (@srijan): why do I have to cast the wallet twice here?
+      Tezos.setWalletProvider(wallet as unknown as WalletProvider);
 
-			walletInfo.setWallet(wallet);
-			const activeAccount = await wallet.client.getActiveAccount();
-			if (activeAccount) {
-				const userAddress = await wallet.getPKH();
-				walletInfo.setAddress(userAddress);
-			}
-		})();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+      walletInfo.setWallet(wallet);
+      const activeAccount = await wallet.client.getActiveAccount();
+      if (activeAccount) {
+        const userAddress = await wallet.getPKH();
+        walletInfo.setAddress(userAddress);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	return (
-		<Button onClick={connectWallet}>
-			<i className="fas fa-wallet"></i>&nbsp; &nbsp; Connect Wallet
-		</Button>
-	);
+  return (
+    <Button onClick={connectWallet}>
+      <i className="fas fa-wallet"></i>&nbsp; &nbsp; Connect Wallet
+    </Button>
+  );
 };
 
 export default ConnectButton;
---
+++ src/components/scripts/generate-hounds.ts
@@ -0,0 +1,74 @@
+async function loadImageFromPath(path: string): Promise<HTMLImageElement> {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.src = path;
+  });
+}
+
+// Returns a random integer
+function randInt(lo: number, hi: number) {
+  return Math.floor(lo + Math.random() * hi);
+}
+
+// Returns a random element from the array `arr`.
+function randChoice<T>(arr: T[]) {
+  return arr[randInt(0, arr.length)];
+}
+
+const partNames = ["base", "eyes", "mouth", "horn"];
+
+// loads all images whose paths are in the array `paths`.
+function loadImages(paths: string[]) {
+  return Promise.all(paths.map(loadImageFromPath));
+}
+
+// A class that manages loading of resources like images that represent body parts of a hound.
+// This is a Singleton class, and must only have one instance.
+class AssetLoader {
+  private parts: HTMLImageElement[][] | null = null;
+  private static numInstances = 0;
+  public static instance = new AssetLoader();
+
+  private constructor() {
+    ++AssetLoader.numInstances;
+    if (AssetLoader.numInstances > 1) {
+      throw new Error("AssetLoader is a Singleton");
+    }
+  }
+
+  async load() {
+    if (this.parts) return this.parts;
+
+    /// TODO: get rid of the magic number `3`
+    this.parts = await Promise.all(
+      partNames.map(partName => {
+        const pathPrefix = `./assets/parts/${partName}`;
+        const paths = [`${pathPrefix}-1.png`, `${pathPrefix}-2.png`, `${pathPrefix}-3.png`];
+        return loadImages(paths);
+      })
+    );
+
+    return this.parts;
+  }
+}
+
+export async function drawRandomHound(ctx: CanvasRenderingContext2D, w: number, h: number) {
+  const parts = await AssetLoader.instance.load();
+
+  ctx.clearRect(0, 0, w, h);
+  parts.forEach(partImgs => {
+    ctx.drawImage(randChoice(partImgs), 0, 0, w, h);
+  });
+}
+
+// returns a function that can render hounds on demand
+export async function getHoundRenderer() {
+  const parts = await AssetLoader.instance.load();
+  return (ctx: CanvasRenderingContext2D, w: number, h: number) => {
+    ctx.clearRect(0, 0, w, h);
+    parts.forEach(partImgs => {
+      ctx.drawImage(randChoice(partImgs), 0, 0, w, h);
+    });
+  };
+}
