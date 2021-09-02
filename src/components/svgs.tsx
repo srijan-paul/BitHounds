1ae98814df6392727c59a0bef5189ad455e6d5c0
srijan-paul <srijannr1@gmail.com>
2021-09-02 23:31:26 +0530

    refactored UI code for home page

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -73,3 +73,23 @@ export function Dog(props: { size: string }): JSX.Element {
     </svg>
   );
 }
+
+export function SVGSlantTop(props: { color?: string }): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill={props.color || "#1a73e8"}
+        fillOpacity="1"
+        d="M0,256L1440,192L1440,320L0,320Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function SVGSlantBottom(props: { color?: string }): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
+      <path fill={props.color || "#1a73e8"} fillOpacity="1" d="M0,160L1440,64L1440,0L0,0Z"></path>
+    </svg>
+  );
+}
