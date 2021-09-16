760856e205a62a2538d1faa84ad849e6df686592
srijan-paul <srijannr1@gmail.com>
2021-09-16 11:41:26 +0530

    moved search bar to it's own page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Book, Code, Dog, User } from "./svgs";
 import "./css/Navbar.css";
 import { Link } from "react-router-dom";
 
-function NavItem(props: { name: string; children: PropTypes.ReactNodeLike }) {
+function NavItem(props: { name: string; link?: string; children: PropTypes.ReactNodeLike }) {
   return (
-    <div className="nav__item">
-      {props.children}
-      <span style={{ fontSize: "1.1rem" }}>{props.name}</span>
-    </div>
+    <Link to={props.link || "/"}>
+      <div className="nav__item">
+        {props.children}
+        <span style={{ fontSize: "1.1rem" }}>{props.name}</span>
+      </div>
+    </Link>
   );
 }
 
 function Navbar(): JSX.Element {
-  const [searchItem, setSearchItem] = useState("");
-
-  function handleChange(e: any) {
-    console.log(e.target.value);
-    setSearchItem(e.target.value);
-  }
   return (
     <div className="navbar">
       <NavItem name="HoundPedia">
@@ -34,27 +30,9 @@ function Navbar(): JSX.Element {
         <Code size="30px" />
       </NavItem>
 
-      <NavItem name="Profile">
+      <NavItem name="People" link="/search">
         <User size="35px" />
       </NavItem>
-
-      <div className="searchbar">
-        <form>
-          <input
-            name="name"
-            placeholder=" Find User"
-            type="text"
-            className="searchTerm"
-            value={searchItem}
-            onChange={handleChange}
-          />
-          <Link to={`/usr/${searchItem}`}>
-            <button type="submit">
-              <i className="fa fa-search"></i>
-            </button>
-          </Link>
-        </form>
-      </div>
     </div>
   );
 }
